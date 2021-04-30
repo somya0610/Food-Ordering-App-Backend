4d6212d8f598ad24527b93039f9fc33f5862c481
Vicky Gupta <vicky.gupta@in.imshealth.com>
2021-04-30 23:16:00 +0530

    Rest API Endpoints3 : Order Controller: Feature implmented to get Coupon by Coupon Name

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
@@ -1,10 +1,7 @@
 package com.upgrad.FoodOrderingApp.api.exception;
 
 import com.upgrad.FoodOrderingApp.api.model.ErrorResponse;
-import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
-import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
-import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
-import com.upgrad.FoodOrderingApp.service.exception.UpdateCustomerException;
+import com.upgrad.FoodOrderingApp.service.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -70,6 +67,20 @@ public class RestExceptionHandler {
             final UpdateCustomerException exp, final WebRequest request) {
         return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(exp.getCode())
                 .message(exp.getErrorMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * Method to handle CouponNotFoundException if user does not exist in the database
+     *
+     * @param excp      - CouponNotFoundException
+     * @param request   - WebRequest
+     * @return          - ResponseEntity (ErrorResponse along with Http status code
+     */
+    @ExceptionHandler(CouponNotFoundException.class)
+    public ResponseEntity<ErrorResponse> couponNotFoundException(
+            CouponNotFoundException excp, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(excp.getCode())
+                .message(excp.getErrorMessage()), HttpStatus.NOT_FOUND);
     }
 
 }
